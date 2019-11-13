775589cec109dfb6ea7c02aa547e473e4925e187
ASoc9264 <1342903581@qq.com>
2019-11-13 16:10:31 +0800

    第一次提交

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+Vue.use(Router)
+
+import Home from '../views/home'
+export default new Router({
+  routes: [
+    {path: '/', component: Home}
+  ]
+})
---
+++ src/views/home.vue
@@ -0,0 +1,23 @@
+<style>
+    /* @import url('../assets/css/home.css'); */
+    .a {
+        font-size: 75px
+    }
+    .b {
+    width: 750px;
+    height: 100px;
+    background-color: #000;
+}
+</style>
+<template>
+<div>
+    <div class="a">00000000000</div>
+    <div class="b"></div>
+</div>
+    
+</template>
+<script>
+export default {
+    
+}
+</script>
